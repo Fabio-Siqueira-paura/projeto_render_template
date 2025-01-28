4eed13b56effbb54573e921bfc0c55ffe87429c6
Fabio-Siqueira-paura <fabinhosiq15@gmail.com>
2025-01-28 11:17:42 -0300

    add rotas e modificando html e css para formatação do texto

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 
 app =  Flask(__name__)
 
-# rotas
+# lista de cores
+lista_cores = ["red",
+               "blue",
+               "#FACADA",
+               "#BABACA",
+               "#00FF00",
+               "aqua",
+              " green",
+              "Yellow",
+              "Pink",
+              "black"]
 
-app.run()
+
+# rotas
+@app.route("/sobre")
+def pag_sobre():
+    cor_fundo = random.choice(lista_cores)
+    return render_template("sobre.html", cor_fundo_html = cor_fundo)
+
+
+
+
+
+
+app.run(debug = True)
